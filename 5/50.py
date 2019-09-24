d7957676bb6b7d41afab5d3562cbe7b10f5f12ba
kenta-morishita <kenta_morishita@albert2005.co.jp>
2019-09-24 15:58:25 +0900

    start 50

---
+++ 5/50.py
@@ -0,0 +1,4 @@
+from download import download
+
+download("http://www.cl.ecei.tohoku.ac.jp/nlp100/data/nlp.txt", "nlp.txt")
+
---
+++ 5/download.py
@@ -0,0 +1,18 @@
+import requests
+
+def download(url, name="downloaded.txt", is_bin=False):
+    try:
+        r = requests.get(url)
+        if is_bin:
+            with open(name, mode="wb") as f:
+                f.write(r.content)
+        else:
+            r.encoding = r.apparent_encoding
+            with open(name, mode="w") as f:
+                f.write(r.text)
+    except requests.exceptions.RequestException as err:
+        print(err)
+
+if __name__ == "__main__":
+    download("http://www.cl.ecei.tohoku.ac.jp/nlp100/data/hightemp.txt", "hightemp.txt")
+
