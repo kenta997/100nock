a4956ce8c8f3d3db0a33a8b7b3c234a627140a90
kenta-morishita <kenta_morishita@albert2005.co.jp>
2019-10-25 12:00:49 +0900

    complete 58

--- 5/58.py
+++ 5/58.py
@@ -6,24 +6,28 @@ root = ET.parse("nlp.txt.xml").getroot()
 
 result = []
 
+def get_text(sentence, idx):
+    return sentence[0][idx - 1][0].text
+
 for sentence in tqdm(root.findall("./document/sentences/")):
     dependencies = [[dep.attrib["type"], dep[0].attrib["idx"], dep[1].attrib["idx"]]
             for dep in sentence.findall("./dependencies[@type='collapsed-dependencies']/")
-            if dep.attrib["type"] == "nsub" or dep.attrib["type"] == "dobj"]
+            if dep.attrib["type"] == "nsubj" or dep.attrib["type"] == "dobj"]
     dependencies = pd.DataFrame(dependencies, columns=["type", "governor", "dependent"])
-    nsub = dependencies.query("type == 'nsub'")
-    dobj = dependencies.query("type == 'dobj'")
-    dependencies = pd.merge(nsub, dobj, on="governor")
+    nsub = dependencies.query("type == 'nsubj'")[["governor", "dependent"]]
+    dobj = dependencies.query("type == 'dobj'")[["governor", "dependent"]]
+    dependencies = pd.merge(nsub, dobj, on="governor", how="outer", suffixes=["_subj", "_obj"])
+    dependencies["subject"] = dependencies["dependent_subj"].apply(
+            lambda x: get_text(sentence, int(x)) if not pd.isna(x) else "")
+    dependencies["predicate"] = dependencies["governor"].apply(lambda x: get_text(sentence, int(x)))
+    dependencies["object"] = dependencies["dependent_obj"].apply(
+            lambda x: get_text(sentence, int(x)) if not pd.isna(x) else "")
+    dependencies["result"] = dependencies["subject"] + "\t" + dependencies["predicate"] + "\t" + dependencies["object"]
+    for x in dependencies["result"].values:
+        result.append(x)
 
-    dot += "".join(["\tnode{} [label=\"{}\"];\n".format(token.attrib["id"], token[0].text)
-        for token in sentence.findall("./tokens/token") if not token[0].text in punct])
-    dot += "".join(["\tnode{} -> node{};\n".format(dep[0].attrib["idx"], dep[1].attrib["idx"])
-        for dep in sentence.findall("./dependencies[@type='collapsed-dependencies']/") if not dep[1].text in punct])
-    dot += "}\n"
+print("\n".join(result))
 
-    with open("dot/{}.dot".format(str(i).zfill(5)), mode="w") as f:
-        f.write(dot)
-
-    graph = pydotplus.graph_from_dot_file("dot/{}.dot".format(str(i).zfill(5)))
-    graph.write_png("png/{}.png".format(str(i).zfill(5)))
+with open("58.text", mode="w") as f:
+    f.write("\n".join(result))
 
