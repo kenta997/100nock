5c6d2eea0a3430dc76be5d8388e81107a7953a03
kenta-morishita <kenta_morishita@albert2005.co.jp>
2019-10-30 15:57:11 +0900

    modify 70

--- 7/70.py
+++ 7/70.py
@@ -18,3 +18,13 @@ random.shuffle(cat)
 with open("sentiment.txt", mode="w", encoding=encoding) as f:
     f.writelines(cat)
 
+pos_num = 0
+neg_num = 0
+for x in cat:
+    if x.startswith("+1"):
+        pos_num += 1
+    elif x.startswith("-1"):
+        neg_num += 1
+
+print("pos: {}, neg: {}".format(pos_num, neg_num))
+
