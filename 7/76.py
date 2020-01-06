b2d1351ad80253b23e12cc4cc2f83bff9c0cb911
kenta-morishita <kenta_morishita@albert2005.co.jp>
2020-01-06 17:52:10 +0900

    complete 76

--- 7/76.py
+++ 7/76.py
@@ -2,4 +2,20 @@ import joblib
 from sklearn.linear_model import LogisticRegression
 
 
+df = joblib.load("features.joblib")
+X = df.drop(columns="class_label")
+y = df["class_label"]
+
+model = joblib.load("model.joblib")
+pred = model.predict(X)
+proba = model.predict_proba(X)
+
+result = "\n".join([
+    "\t".join([str(sentence[0]), str(sentence[1]), str(sentence[2])]) for sentence in zip(y, pred, proba)
+    ])
+
+with open("labeling.txt", mode="w", encoding="cp1252") as f:
+    f.writelines(result)
+
+print("".join(result))
 
