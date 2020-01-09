f9d4ef6cc5adbea8dc4311527aeb5b50303d0cd2
kenta-morishita <kenta_morishita@albert2005.co.jp>
2020-01-09 11:47:34 +0900

    complete 89

--- 8/89.py
+++ 8/89.py
@@ -6,8 +6,8 @@ from tqdm import tqdm
 tqdm.pandas(leave=False)
 
 X = pd.read_csv("X300.csv", header=None, index_col=0, quoting=2, keep_default_na=False)
-eng = X.loc["England"]
-cos = X.progress_apply(lambda x: np.dot(eng, x) / (np.linalg.norm(eng) * np.linalg.norm(x)), axis=1
+v = X.loc["Spain"] - X.loc["Madrid"] + X.loc["Athens"]
+cos = X.progress_apply(lambda x: np.dot(v, x) / (np.linalg.norm(v) * np.linalg.norm(x)), axis=1
         ).fillna(-1).sort_values(ascending=False)
-print(cos[1:11])
+print(cos[:10])
 
