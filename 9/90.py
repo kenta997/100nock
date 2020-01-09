58c55b980bb1022a6d87b7e60214c36169e195d5
kenta-morishita <kenta_morishita@albert2005.co.jp>
2020-01-09 14:32:20 +0900

    modify 90

--- 9/90.py
+++ 9/90.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import csv
 from gensim.models import word2vec
 import logging
 
@@ -5,6 +7,8 @@ import logging
 logging.basicConfig(format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO)
 sentences = word2vec.Text8Corpus("../8/81.txt")
 
-model = word2vec.Word2Vec(sentences, size=300, min_count=10, window=5)
+model = word2vec.Word2Vec(sentences, size=300, min_count=5, window=5)
 model.save("word2vec.model")
 
+X = pd.DataFrame(model.wv.vectors, index=model.wv.index2word).sort_index()
+X.to_csv("X300.csv", index=True, header=False, quoting=csv.QUOTE_NONNUMERIC)
